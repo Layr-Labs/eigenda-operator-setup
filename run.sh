ff592247997f4e7bf4e3eead4a37c9ed3f60b196
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-12-15 11:13:40 -0800

    update plugin image

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ optIn() {
   --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
   --volume "${NODE_BLS_KEY_FILE_HOST}":/app/operator_keys/bls_key.json \
   --volume "${NODE_LOG_PATH_HOST}":/app/logs:rw \
-  ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.1.1 \
+  ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.1.3 \
   --operation opt-in \
   --socket "$socket"
 }
@@ -22,7 +22,7 @@ optOut() {
     --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
     --volume "${NODE_BLS_KEY_FILE_HOST}":/app/operator_keys/bls_key.json \
     --volume "${NODE_LOG_PATH_HOST}":/app/logs:rw \
-    ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.1.1 \
+    ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.1.3 \
     --operation opt-out \
     --socket "0"
 }
