01668f031ee75c0c2b90adf69c9a6a81dc032c94
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-12-21 15:41:56 -0800

    add release 0.2.0 images (#30)

--- run.sh
+++ run.sh
@@ -11,20 +11,21 @@ optIn() {
   --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
   --volume "${NODE_BLS_KEY_FILE_HOST}":/app/operator_keys/bls_key.json \
   --volume "${NODE_LOG_PATH_HOST}":/app/logs:rw \
-  ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.1.3 \
+  ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.2.0 \
   --operation opt-in \
   --socket "$socket"
 }
 
 optOut() {
+  socket="$NODE_HOSTNAME":"${NODE_DISPERSAL_PORT}"\;"${NODE_RETRIEVAL_PORT}"
   docker run --env-file .env \
     --rm \
     --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
     --volume "${NODE_BLS_KEY_FILE_HOST}":/app/operator_keys/bls_key.json \
     --volume "${NODE_LOG_PATH_HOST}":/app/logs:rw \
-    ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.1.3 \
+    ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.2.0 \
     --operation opt-out \
-    --socket "0"
+    --socket "$socket"
 }
 
 if [ "$1" = "opt-in" ]; then
