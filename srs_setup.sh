cf1a8fc5812ada01053e61e8c47561639eaa0100
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-02-05 15:02:58 -0800

    script updates

--- srs_setup.sh
+++ srs_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Path: srs_setup.sh
 
+echo "Downloading srs resources"
+echo "Downloading g1 point. This could take upto 5 minutes"
 wget https://srs-mainnet.s3.amazonaws.com/kzg/g1.point --output-document=./resources/g1.point
+echo "Downloading g2 point. This could take upto 10 minutes"
 wget https://srs-mainnet.s3.amazonaws.com/kzg/g2.point --output-document=./resources/g2.point
 
-sha256sum -c ./resources/srssha256sums.txt
+echo "validating hashes of g1 and g2 points This could take upto 5 minutes"
+cd resources && sha256sum -c srssha256sums.txt
