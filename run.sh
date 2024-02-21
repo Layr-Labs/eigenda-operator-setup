e4b8550851fe95fc734ed8cc830e3449aef29374
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-02-21 14:28:26 -0800

    add update quorum

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 
 . ./.env
 
-# In both opt-in and opt-out, We have to explicitly set the password again here because
+socket="$NODE_HOSTNAME":"${NODE_DISPERSAL_PORT}"\;"${NODE_RETRIEVAL_PORT}"
+
+# In both all commands, We have to explicitly set the password again here because
 # when docker run loads the `.env` file, it keeps the quotes around the password
 # which causes the password to be incorrect.
 # To test that try running `docker run --rm --env-file .env busybox /bin/sh -c 'echo $NODE_ECDSA_KEY_PASSWORD'`
@@ -15,7 +17,6 @@ optIn() {
     echo "Error: SRS setup failed. Exiting."
     exit 1
   fi
-  socket="$NODE_HOSTNAME":"${NODE_DISPERSAL_PORT}"\;"${NODE_RETRIEVAL_PORT}"
   echo "using socket: $socket"
   docker run --env-file .env \
   --rm \
@@ -30,7 +31,6 @@ optIn() {
 }
 
 optOut() {
-  socket="$NODE_HOSTNAME":"${NODE_DISPERSAL_PORT}"\;"${NODE_RETRIEVAL_PORT}"
   docker run --env-file .env \
     --rm \
     --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
@@ -43,10 +43,26 @@ optOut() {
     --socket "$socket"
 }
 
+updateQuorums() {
+    echo "using socket: $socket"
+    docker run --env-file .env \
+      --rm \
+      --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
+      --volume "${NODE_BLS_KEY_FILE_HOST}":/app/operator_keys/bls_key.json \
+      --volume "${NODE_LOG_PATH_HOST}":/app/logs:rw \
+      ghcr.io/layr-labs/eigenda/opr-nodeplugin:release-0.3.0 \
+      --ecdsa-key-password "$NODE_ECDSA_KEY_PASSWORD" \
+      --bls-key-password "$NODE_BLS_KEY_PASSWORD" \
+      --operation update-quorums \
+      --socket "$socket"
+}
+
 if [ "$1" = "opt-in" ]; then
   optIn
 elif [ "$1" = "opt-out" ]; then
   optOut
+elif [ "$1" = "update-quorums" ]; then
+  updateQuorums
 else
   echo "Invalid command"
 fi
