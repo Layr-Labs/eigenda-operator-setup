be9443613b084ce420cd89b0f17ba57aefb8d409
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-04-18 18:36:41 -0700

    missed commit

--- srs_setup.sh
+++ srs_setup.sh
@@ -6,7 +6,7 @@ echo "Downloading srs resources"
 if ! [ -f ./resources/g1.point ]; then
   echo "g1.point does not exist."
   echo "Downloading g1 point. This could take upto 5 minutes"
-  wget https://srs-mainnet.s3.amazonaws.com/kzg/g1.point --output-document=../resources/g1.point
+  wget https://srs-mainnet.s3.amazonaws.com/kzg/g1.point --output-document=./resources/g1.point
   DOWNLOADED_FILE=true
 else
   echo "g1.point already exists."
@@ -15,7 +15,7 @@ fi
 if ! [ -f ./resources/g2.point.powerOf2 ]; then
   echo "g2.point.powerOf2 does not exist."
   echo "Downloading g2 point powerOf2. This will take few seconds"
-  wget https://srs-mainnet.s3.amazonaws.com/kzg/g2.point.powerOf2 --output-document=../resources/g2.point.powerOf2
+  wget https://srs-mainnet.s3.amazonaws.com/kzg/g2.point.powerOf2 --output-document=./resources/g2.point.powerOf2
   DOWNLOADED_FILE=true
 else
   echo "g2.point.powerOf2 already exists."
